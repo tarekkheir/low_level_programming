1752f4faa5015223776b059fa42a9e85e020e9ab
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-16 15:31:39 +0000

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char charactere;
+int entier;
+long entier_long;
+long long entier_long_long;
+float flottant;
+printf("Size of a char: %ld byte(s)\n", sizeof(charactere));
+printf("Size of an int: %ld byte(s)\n", sizeof(entier));
+printf("Size of a long int: %ld byte(s)\n", sizeof(entier_long));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(entier_long_long));
+printf("Size of a flottant: %ld byte(s)\n", sizeof(flottant));
+return (0);
+}
