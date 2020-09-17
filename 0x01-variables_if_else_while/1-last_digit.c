3e4ab86170d80ea5f0bd17a342e06a9f4c67337b
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-17 10:10:26 +0000

    Loop

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n, lastdigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastdigit = n % 10;
+/* your code goes there */
+if (lastdigit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+}
+else if (lastdigit == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+}
+else if (lastdigit < 6 && lastdigit != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 97;
+for (a = 97; a <= 122; a++)
+{
+putchar((char)a);
+}
+putchar('\n');
+return (0);
+}
