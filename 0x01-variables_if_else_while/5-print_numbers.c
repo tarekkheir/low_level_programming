565558a8d27ecde3c2d4e526a82d00f346a39f10
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-17 12:40:07 +0000

    Loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 0;
+for (a = 0; a <= 9; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 122;
+for (a = 122; a >= 97; a--)
+{
+putchar((char)a);
+}
+putchar('\n');
+return (0);
+}
