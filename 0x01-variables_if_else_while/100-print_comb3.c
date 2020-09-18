a8520f7374dad00acacf35adb904edb3461c11a9
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-18 08:29:00 +0000

    Loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int a, b = '0';
+  for (b = '0'; b <= '9'; b++)
+    {
+      for (a = '0'; a <= '9'; a++)
+	{
+	  if (b < a)
+	    {
+	  putchar(b);
+	  putchar(a);
+	    }
+	  if (b == '8' && a == '9')
+	    {
+	      break;
+	    }
+	  if (b < a)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+  putchar('\n');
+  return (0);
+}
