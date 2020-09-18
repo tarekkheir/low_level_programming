33989cc7369756d246a4c6f9e31860e70e2c3330
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-18 09:43:29 +0000

    Loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-  int a, b = '0';
-  for (b = '0'; b <= '9'; b++)
-    {
-      for (a = '0'; a <= '9'; a++)
-	{
-	  if (b < a)
-	    {
-	  putchar(b);
-	  putchar(a);
-	    }
-	  if (b == '8' && a == '9')
-	    {
-	      break;
-	    }
-	  if (b < a)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int a, b = '0';
+for (b = '0'; b <= '9'; b++)
+{
+for (a = '0'; a <= '9'; a++)
+{
+if (b < a)
+{
+putchar(b);
+putchar(a);
+}
+if (b == '8' && a == '9')
+{
+break;
+}
+if (b < a)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
