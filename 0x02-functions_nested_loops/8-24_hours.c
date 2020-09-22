ff105c787cb8a7ba73dd67b4cac0d1d15187ba8f
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-22 20:19:24 +0000

    Function nested loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- *jack_bauer - check code
- *Return: always 0
+ * jack_bauer - check code
+ * Return: always 0
  */
 void jack_bauer(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 24; a++)
+	for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 59; b++)
 		{
