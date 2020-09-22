1e256cf83644c4a1a21ee96b161b396959f8dc3e
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-22 10:38:37 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -14,3 +17,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
