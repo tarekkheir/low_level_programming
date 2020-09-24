a555d94fc89a20e4420e61907b0ccfa837577c09
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-24 13:30:32 +0000

    More

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *_isdigit - check the code
+ *
+ *@c: letter that we use
+ *Return: always 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+
+	{
+
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+	return (0);
+}
