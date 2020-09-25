648f5c4b81b3791f3834aa45649cee9faebc45f4
Tarek Kheir <kheirtarek@hotmail.fr>
2020-09-25 16:16:42 +0000

    Function nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,23 @@ void times_table(void)
 
 		for (b = 0; b <= 9; b++)
 		{
+
 			if ((b * a) / 10 != 0)
 			{
-			_putchar('0' + (b * a) / 10);
+				_putchar('0' + (b * a) / 10);
 			}
-			else
+
+			else if (b != 0)
 			{
 				_putchar(' ');
 			}
+
 			_putchar('0' + (b * a) % 10);
+
 			if (b < 9)
 			{
-			_putchar(',');
-			_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
