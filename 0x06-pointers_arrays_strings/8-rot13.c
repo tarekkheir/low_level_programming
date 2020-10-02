076d5e11df02b29c34913cca834f7fe1e786bd41
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-02 08:25:36 +0000

    more pointers

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,19 +9,20 @@ char *rot13(char *s)
 	char a[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char b[53] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	int x = 0;
-	int y = 0;
+	int y;
 
 	while (s[x] != '\0')
 	{
-	while (s[x] != a[y])
-	{
-		y++;
-	}
-	if (y <= 51)
-	{
-		s[x] = b[y];
-	}
-	x++;
+		y = 0;
+		while (s[x] != a[y])
+		{
+			y++;
+		}
+		if (y <= 52)
+		{
+			s[x] = b[y];
+		}
+		x++;
 	}
 	return (s);
 }
