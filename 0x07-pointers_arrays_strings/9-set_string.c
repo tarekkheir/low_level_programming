e3dbf5ac0b39b560e37c7a14617a57b59e796140
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-06 17:33:52 +0000

    more pointers

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *set_string - check code
+ *@s: letter
+ *@to: letter
+ *Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+/*	int i = 0;
+	char tmp;
+
+	while (to[i])
+	{
+		tmp = *s[i];
+		*s[i] = to[i];
+		to[i] = tmp;
+		i++;
+		}*/
+}
