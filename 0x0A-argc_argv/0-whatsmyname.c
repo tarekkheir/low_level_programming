6a3925843d851e0e4b1a172283ee0e808c8e9cc7
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-11 12:14:58 +0000

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ *main - check code
+ *@argc: letter
+ *@argv: letter
+ *Return: value
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 0)
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
