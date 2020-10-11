9a45c765308b723a97719c8e0eb9851ab2a408c4
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-11 13:05:08 +0000

    argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ *main - check code
+ *@argc: letter
+ *@argv: letter
+ *Return: value
+ */
+int main(int argc, char *argv[])
+{
+	int tmp = 0;
+	int a = 1;
+
+	while (a < argc)
+	{
+		if (! atoi(argv[a]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		if (atoi(argv[a]) >= 0)
+		{
+			tmp += atoi(argv[a]);
+		}
+		a++;
+	}
+	printf("%d\n", tmp);
+	return (0);
+}
