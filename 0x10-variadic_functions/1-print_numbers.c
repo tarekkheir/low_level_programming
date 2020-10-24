cdf44369f883c68d31c1a50c34099c6d0a74568e
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-24 12:20:18 +0000

    variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ *print_numbers - check code
+ *@separator: letter
+ *@n: letter
+ *Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i = 0;
+
+	va_start(numbers, n);
+
+	while (i < n)
+	{
+		printf("%d", va_arg(numbers, int));
+		i++;
+		if (i < n)
+			printf("%s", separator);
+		else
+			printf("\n");
+	}
+	va_end(numbers);
+}
