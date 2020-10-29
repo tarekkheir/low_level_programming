b135499d8ee4b204273d8333d70078974840ea2a
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-29 20:30:28 +0000

    linked list

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct element_t
+{
+	const char *str;
+	unsigned int len;
+	struct element_t *next;
+} list_t, Element;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+unsigned int _strlen(const char *s);
+unsigned int _strlen_list(list_t *list);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
