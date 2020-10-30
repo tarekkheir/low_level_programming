b9f8039561f3a2b4e842a28fde838600139eb7a2
Tarek Kheir <kheirtarek@hotmail.fr>
2020-10-30 14:40:47 +0000

    linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 void print_first(void) __attribute__ ((constructor));
-
+/**
+ *print_first - print before main
+ *void
+ */
 void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
