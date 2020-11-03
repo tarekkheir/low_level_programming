53b78726f080b46ff9309003510e7b6ca2d801ba
Tarek Kheir <kheirtarek@hotmail.fr>
2020-11-03 18:39:36 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ *listint_len - length of list
+ *@h: list
+ *Return: size
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+/**
  *insert_nodeint_at_index - read the name of function :)
  *@head: list
  *@idx: index
@@ -13,7 +30,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *act = *head;
 	unsigned int i = 0;
 
+	if (cell == NULL)
+		return (NULL);
+
 	cell->n = n;
+
+	if (idx > listint_len(*head))
+		return (NULL);
 
 	while (i < idx)
 	{
