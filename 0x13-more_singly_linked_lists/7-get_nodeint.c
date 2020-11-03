9b18144c9185d13f7366d334b0dc1df6506ec522
Tarek Kheir <kheirtarek@hotmail.fr>
2020-11-03 18:33:34 +0000

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ *listint_len - length of list
+ *@h: list
+ *Return: size
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+/**
  *get_nodeint_at_index - as the name of function said
  *@head: list
  *@index: index
@@ -9,6 +26,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *result;
 	unsigned int i = 0;
+
+	if (index > listint_len(head))
+		return (NULL);
 
 	while (i < index)
 	{
