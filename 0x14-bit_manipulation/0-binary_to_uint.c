f3515b96b8a4dd77359d2d86b50ea9a0a184428e
Tarek Kheir <kheirtarek@hotmail.fr>
2020-11-05 09:56:09 +0000

    Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+unsigned int _pow(unsigned int n, int pow)
+{
+	if (pow == 0)
+		return (1);
+	if (pow == 1)
+		return (n);
+	n = n * 2;
+	return (_pow(n, pow - 1));
+}
+/**
+ *_strlen - length of pointer
+ *
+ *@s: letter that we use
+ *Return: always 0
+ */
+int _strlen(const char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+/**
+ *binary_to_uint - convert integer into binary
+ *@b: string
+ *Return: integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int pui = 0;
+	unsigned int result = 0;
+	int len;
+
+	len = _strlen(b);
+	if (len == 0)
+		return (0);
+	len--;
+	while (len >= 0)
+	{
+		if (b[len] == 48)
+		{
+			len--;
+			pui++;
+		}
+		else if (b[len] == 49)
+		{
+			result = result + _pow(2, pui);
+			pui++;
+			len--;
+		}
+		else
+			return (0);
+
+	}
+	return (result);
+}
