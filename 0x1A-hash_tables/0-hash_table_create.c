47c308822024681106b382f704c7352581900bab
Tarek <kheirtarek@hotmail.fr>
2021-01-15 15:43:27 +0100

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = malloc(sizeof(ht));
+	ht = malloc(sizeof(hash_table_t *));
 	if (ht == NULL)
 		return (NULL);
 
